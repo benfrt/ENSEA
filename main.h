5a97a2f6286574ebd65551fd62aaf37f5599e8ad
benfrt <122082349+benfrt@users.noreply.github.com>
2023-01-06 12:04:45 +0100

    Create main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+
+#define MSG_WELCOME  "Bienvenue dans le shell ENSEA.\nPour quitter tapez 'exit'.\n"
+#define MSG_PROMPT "enseah %"
+#define MSG_FORTUNE "YOU HAVE A BAD LUCK"
+#define PATH   "/home"
+#define test "main.c"
+
+
