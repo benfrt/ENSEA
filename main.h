9b976fdbf5540257e87f47530eed74b1af37035a
benfrt <122082349+benfrt@users.noreply.github.com>
2023-01-13 10:36:03 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,14 +1,23 @@
-
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
 
 
 #define MSG_WELCOME  "Bienvenue dans le shell ENSEA.\nPour quitter tapez 'exit'.\n"
 #define MSG_PROMPT "enseah %"
-#define MSG_FORTUNE "YOU HAVE A BAD LUCK"
+#define EXIT_FAILURE_MSG "YOU HAVE A BAD LUCK ! TRY AGAIN...\n"
+
+
+#define EXIT_MSG "Good bye, have a great time.\n"
+
+#define MSG_NEXT_PROMPT_SIGN "enseash [ sign : %d| %d ms ]%%"
+#define MSG_NEXT_PROMPT_EXIT "enseash [ exit : %d| %d ms ]%%"
+
+#define CMD_SIZE 128
+
 #define PATH   "/home"
 #define test "main.c"
-
-
